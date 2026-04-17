55792ff53315b36b6778a89f770c6593b20930c2
LuisBandivas <tbsi.luis@gmail.com>
2026-04-17 17:22:59 +0800

    design : added UI for Email setup configuration page/cvu

---
+++ Web.BlazorServer/Components/Pages/Configuration/Setup/EmailSetup/EmailManagementCVU.razor.cs
@@ -0,0 +1,178 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Components;
+using Web.BlazorServer.Defaults;
+using Web.BlazorServer.Handlers.Repositories.Configuration.Setup.Email;
+using Web.BlazorServer.Helpers;
+using Web.BlazorServer.ViewModels.Configuration.Setup.Email;
+using Web.BlazorServer.ViewModels.Enums;
+using KernelEnumHelper = Shared.Kernel.EnumHelper;
+
+namespace Web.BlazorServer.Components.Pages.Configuration.Setup.EmailSetup;
+
+public partial class EmailManagementCVU
+{
+    #region Parameters
+    [SupplyParameterFromQuery]
+    [Parameter]
+    public Guid Ref { get; set; } = Guid.NewGuid();
+
+    [Parameter]
+    public bool ModalMode { get; set; } = false;
+
+    [Parameter]
+    public PageActionTypeEnum? ModalAction { get; set; } = null;
+    #endregion Parameters
+
+    #region Injects
+    [Inject] IEmailManagementHandler EmailManagementHandler { get; set; } = default!;
+    #endregion Injects
+
+    #region Primitives
+    PageActionTypeEnum PageAction { get; set; }
+    bool PasswordVisibility { get; set; } = false;
+    bool Creating => PageAction == PageActionTypeEnum.Create;
+    bool Updating => PageAction == PageActionTypeEnum.Update;
+    bool Viewing => PageAction == PageActionTypeEnum.View;
+    bool IsBusy => AppBusyService.IsBusy(ActionCreateEmail) || AppBusyService.IsBusy(ActionGetEmail) || AppBusyService.IsBusy(ActionUpdateEmail);
+    bool IsLoadingData => AppBusyService.IsBusy(ActionGetEmail);
+
+    readonly string ActionCreateEmail = KernelEnumHelper.GetEnumDescription(AppActions.CreateEmail);
+    readonly string ActionGetEmail = KernelEnumHelper.GetEnumDescription(AppActions.ViewEmail);
+    readonly string ActionUpdateEmail = KernelEnumHelper.GetEnumDescription(AppActions.UpdateEmail);
+    #endregion Primitives
+
+    #region Custom Classes
+    EmailSetupVM? Email { get; set; } = null;
+    #endregion Custom Classes
+
+    #region Overrides
+    protected override void OnParametersSet()
+    {
+        if (!ModalMode)
+            PageAction = PageActionHelper.GetPageActionType(NavManager.Uri);
+        else
+        {
+            if (ModalMode && ModalAction != null)
+                PageAction = (PageActionTypeEnum)ModalAction;
+        }
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        AppBusyService.SetBusy(ActionGetEmail, true);
+        if (!Can.Do("OEMS", KernelEnumHelper.GetEnumDescription(PageAction)))
+            NavManager.NavigateTo("/401", true);
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            await LoadDataAsync();
+        }
+    }
+    protected override async Task CancelEditing()
+    {
+        if (UnsavedChangesService.HasChanges)
+            if (!await AlertService.HasUnsavedChangesAsync(header: "Cancel Email Update"))
+                return;
+        NavManager.NavigateTo($"/configuration/setup/Email-setup/view?ref={Ref}", true);
+    }
+
+    protected override async Task HandleSubmit()
+    {
+        if (Creating)
+            await Create();
+        if (Updating)
+            await Update();
+    }
+
+    protected override async Task InitializeEditing()
+    {
+        NavManager.NavigateTo($"/configuration/setup/email-setup/update?ref={Ref}", true);
+    }
+    #endregion Overrides
+
+
+    #region Custom Functions
+    async Task ToggleVisibility()
+    {
+        PasswordVisibility = !PasswordVisibility;
+        await InvokeAsync(StateHasChanged);
+    }
+
+    async Task LoadDataAsync()
+    {
+        if (Viewing || Updating)
+        {
+            var action = await AppActionFactory.RunAsync(async () =>
+            {
+                AppBusyService.SetBusy(ActionGetEmail, true);
+
+                var Email = await EmailManagementHandler.GetEmailAsync(Ref);
+                Email.Adapt(FormData);
+
+                AppBusyService.SetBusy(ActionGetEmail, false);
+
+            }, AppActionOptionPresets.Loading(ActionGetEmail));
+        }
+        else
+        {
+            AppBusyService.SetBusy(ActionGetEmail, false);
+        }
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    async Task Create()
+    {
+        var action = await AppActionFactory.RunAsync(async () =>
+        {
+            AppBusyService.SetBusy(ActionCreateEmail, true);
+
+            var result = await EmailManagementHandler.CreateEmailAsync(FormData);
+
+            AppBusyService.SetBusy(ActionCreateEmail, false);
+            return result;
+
+        }, AppActionOptionPresets.Confirmed(ActionCreateEmail));
+
+        action.OnSuccess(async (args) =>
+        {
+            UnsavedChangesService.MarkClean();
+            await Return();
+        });
+    }
+
+    async Task Update()
+    {
+        var action = await AppActionFactory.RunAsync(async () =>
+        {
+            AppBusyService.SetBusy(ActionUpdateEmail, true);
+
+            var result = await EmailManagementHandler.UpdateEmailAsync(FormData);
+
+            AppBusyService.SetBusy(ActionUpdateEmail, false);
+            return result;
+
+        }, AppActionOptionPresets.Confirmed(ActionUpdateEmail));
+
+        action.OnSuccess(async (args) =>
+        {
+            UnsavedChangesService.MarkClean();
+            await Return();
+        });
+    }
+
+    async Task Return()
+    {
+        if (UnsavedChangesService.HasChanges)
+            if (!await AlertService.HasUnsavedChangesAsync(header: "Cancel Email Setup"))
+                return;
+
+        NavManager.NavigateTo($"/configuration/setup/email-setup", true);
+    }
+    #endregion Custom Functions
+}
---
+++ Web.BlazorServer/Components/Pages/Configuration/Setup/EmailSetup/EmailManagementPage.razor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+using Shared.Entities;
+using Shared.Kernel;
+using Web.BlazorServer.Components.Shared.Abstraction;
+using Web.BlazorServer.Defaults;
+using Web.BlazorServer.Handlers.Repositories.Configuration.Setup.Email;
+using Web.BlazorServer.Services.Repositories;
+using Web.BlazorServer.ViewModels.Abstraction;
+using Web.BlazorServer.ViewModels.Configuration.Setup.Email;
+
+namespace Web.BlazorServer.Components.Pages.Configuration.Setup.EmailSetup;
+
+public partial class EmailManagementPage
+{
+    [Inject] IGridSettingsService GridSettingsService { get; set; } = default!;
+    [Inject] IEmailManagementHandler EmailManagementHandler { get; set; } = default!;
+
+    AppDataGrid<EmailDataGridVM> EmailsDataGrid { get; set; }
+    DataGridSettings EmailsDataGridSettings { get; set; }
+
+    string ActionGetEmails { get; } = EnumHelper.GetEnumDescription(AppActions.GetAllEmails);
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            await LoadGridSettings();
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+
+    async Task LoadGridSettings()
+    {
+        await GridSettingsService.SetGridSettings(EmailsDataGrid.DataGrid, settings => EmailsDataGridSettings = settings ?? new());
+        GridSettingsLoaded = true;
+
+        await EmailsDataGrid.DataGrid.ReloadSettings();
+        await EmailsDataGrid.DataGrid.Reload();
+    }
+
+    async Task<DataGridResultVM<EmailDataGridVM>> LoadDataAsync(DataGridIntent intent)
+    {
+        var action = await AppActionFactory.RunAsync(async () =>
+        {
+            AppBusyService.SetBusy(ActionGetEmails, true);
+
+            var response = await EmailManagementHandler.GetAllEmailsAsync(intent);
+
+            return response;
+
+        }, AppActionOptionPresets.Loading(ActionGetEmails));
+
+        AppBusyService.SetBusy(ActionGetEmails, false);
+        return DataGridResultVM<EmailDataGridVM>.New(action.Result.data ?? [], action.Result.count);
+    }
+
+    void CreateEmail() => NavManager.NavigateTo("/configuration/setup/email-setup/create", true);
+    void ViewEmail(EmailDataGridVM email) => NavManager.NavigateTo($"/configuration/setup/email-setup/view?ref={email.Id}", true);
+}
