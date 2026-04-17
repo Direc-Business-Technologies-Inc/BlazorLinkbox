f7c033320756bb35c517d989364c7056be501f36
LuisBandivas <tbsi.luis@gmail.com>
2026-04-17 17:23:32 +0800

    design : added UI for SAP setup configuration page/cvu

---
+++ Web.BlazorServer/Components/Pages/Configuration/Setup/ApiSetup/ApiManagementCVU.razor.cs
@@ -0,0 +1,180 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Components;
+using Web.BlazorServer.Defaults;
+using Web.BlazorServer.Handlers.Repositories.Configuration.Setup.Api;
+using Web.BlazorServer.Helpers;
+using Web.BlazorServer.Services.Implementation;
+using Web.BlazorServer.ViewModels.Configuration.Setup.Api;
+using Web.BlazorServer.ViewModels.Enums;
+using KernelEnumHelper = Shared.Kernel.EnumHelper;
+
+namespace Web.BlazorServer.Components.Pages.Configuration.Setup.ApiSetup;
+
+public partial class ApiManagementCVU
+{
+    #region Parameters
+    [SupplyParameterFromQuery]
+    [Parameter]
+    public Guid Ref { get; set; } = Guid.NewGuid();
+
+    [Parameter]
+    public bool ModalMode { get; set; } = false;
+
+    [Parameter]
+    public PageActionTypeEnum? ModalAction { get; set; } = null;
+    #endregion Parameters
+
+    #region Injects
+    [Inject] IApiManagementHandler ApiManagementHandler { get; set; } = default!;
+    #endregion Injects
+
+    #region Primitives
+    PageActionTypeEnum PageAction { get; set; }
+    bool PasswordVisibility { get; set; } = false;
+    bool HiddenFields { get; set; } = false;
+    bool Creating => PageAction == PageActionTypeEnum.Create;
+    bool Updating => PageAction == PageActionTypeEnum.Update;
+    bool Viewing => PageAction == PageActionTypeEnum.View;
+    bool IsBusy => AppBusyService.IsBusy(ActionCreateApi) || AppBusyService.IsBusy(ActionGetApi) || AppBusyService.IsBusy(ActionUpdateApi);
+    bool IsLoadingData => AppBusyService.IsBusy(ActionGetApi);
+
+    readonly string ActionCreateApi = KernelEnumHelper.GetEnumDescription(AppActions.CreateApi);
+    readonly string ActionGetApi = KernelEnumHelper.GetEnumDescription(AppActions.ViewApi);
+    readonly string ActionUpdateApi = KernelEnumHelper.GetEnumDescription(AppActions.UpdateApi);
+    #endregion Primitives
+
+    #region Custom Classes
+    ApiSetupVM? Api { get; set; } = null;
+    #endregion Custom Classes
+
+    #region Overrides
+    protected override void OnParametersSet()
+    {
+        if (!ModalMode)
+            PageAction = PageActionHelper.GetPageActionType(NavManager.Uri);
+        else
+        {
+            if (ModalMode && ModalAction != null)
+                PageAction = (PageActionTypeEnum)ModalAction;
+        }
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        AppBusyService.SetBusy(ActionGetApi, true);
+        if (!Can.Do("OPTS", KernelEnumHelper.GetEnumDescription(PageAction)))
+            NavManager.NavigateTo("/401", true);
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            await LoadDataAsync();
+        }
+    }
+    protected override async Task CancelEditing()
+    {
+        if (UnsavedChangesService.HasChanges)
+            if (!await AlertService.HasUnsavedChangesAsync(header: "Cancel Api Update"))
+                return;
+        NavManager.NavigateTo($"/configuration/setup/Api-setup/view?ref={Ref}", true);
+    }
+
+    protected override async Task HandleSubmit()
+    {
+        if (Creating)
+            await Create();
+        if (Updating)
+            await Update();
+    }
+
+    protected override async Task InitializeEditing()
+    {
+        NavManager.NavigateTo($"/configuration/setup/Api-setup/update?ref={Ref}", true);
+    }
+    #endregion Overrides
+
+
+    #region Custom Functions
+    async Task ToggleVisibility()
+    {
+        PasswordVisibility = !PasswordVisibility;
+        await InvokeAsync(StateHasChanged);
+    }
+
+    async Task LoadDataAsync()
+    {
+        if (Viewing || Updating)
+        {
+            var action = await AppActionFactory.RunAsync(async () =>
+            {
+                AppBusyService.SetBusy(ActionGetApi, true);
+
+                var Api = await ApiManagementHandler.GetApiAsync(Ref);
+                Api.Adapt(FormData);
+
+                AppBusyService.SetBusy(ActionGetApi, false);
+
+            }, AppActionOptionPresets.Loading(ActionGetApi));
+        }
+        else
+        {
+            AppBusyService.SetBusy(ActionGetApi, false);
+        }
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    async Task Create()
+    {
+        var action = await AppActionFactory.RunAsync(async () =>
+        {
+            AppBusyService.SetBusy(ActionCreateApi, true);
+
+            var result = await ApiManagementHandler.CreateApiAsync(FormData);
+
+            AppBusyService.SetBusy(ActionCreateApi, false);
+            return result;
+
+        }, AppActionOptionPresets.Confirmed(ActionCreateApi));
+
+        action.OnSuccess(async (args) =>
+        {
+            UnsavedChangesService.MarkClean();
+            await Return();
+        });
+    }
+
+    async Task Update()
+    {
+        var action = await AppActionFactory.RunAsync(async () =>
+        {
+            AppBusyService.SetBusy(ActionUpdateApi, true);
+
+            var result = await ApiManagementHandler.UpdateApiAsync(FormData);
+
+            AppBusyService.SetBusy(ActionUpdateApi, false);
+            return result;
+
+        }, AppActionOptionPresets.Confirmed(ActionUpdateApi));
+
+        action.OnSuccess(async (args) =>
+        {
+            UnsavedChangesService.MarkClean();
+            await Return();
+        });
+    }
+
+    async Task Return()
+    {
+        if (UnsavedChangesService.HasChanges)
+            if (!await AlertService.HasUnsavedChangesAsync(header: "Cancel Api Setup"))
+                return;
+
+        NavManager.NavigateTo($"/configuration/setup/Api-setup", true);
+    }
+    #endregion Custom Functions
+}
---
+++ Web.BlazorServer/Components/Pages/Configuration/Setup/ApiSetup/ApiManagementPage.razor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+using Shared.Entities;
+using Shared.Kernel;
+using Web.BlazorServer.Components.Shared.Abstraction;
+using Web.BlazorServer.Defaults;
+using Web.BlazorServer.Handlers.Repositories.Configuration.Setup.Api;
+using Web.BlazorServer.Services.Repositories;
+using Web.BlazorServer.ViewModels.Abstraction;
+using Web.BlazorServer.ViewModels.Configuration.Setup.Api;
+
+namespace Web.BlazorServer.Components.Pages.Configuration.Setup.ApiSetup;
+
+public partial class ApiManagementPage
+{
+    [Inject] IGridSettingsService GridSettingsService { get; set; } = default!;
+    [Inject] IApiManagementHandler ApiManagementHandler { get; set; } = default!;
+
+    AppDataGrid<ApiDataGridVM> ApisDataGrid { get; set; }
+    DataGridSettings ApisDataGridSettings { get; set; }
+
+    string ActionGetApis { get; } = EnumHelper.GetEnumDescription(AppActions.GetAllApis);
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            await LoadGridSettings();
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+
+    async Task LoadGridSettings()
+    {
+        await GridSettingsService.SetGridSettings(ApisDataGrid.DataGrid, settings => ApisDataGridSettings = settings ?? new());
+        GridSettingsLoaded = true;
+
+        await ApisDataGrid.DataGrid.ReloadSettings();
+        await ApisDataGrid.DataGrid.Reload();
+    }
+
+    async Task<DataGridResultVM<ApiDataGridVM>> LoadDataAsync(DataGridIntent intent)
+    {
+        var action = await AppActionFactory.RunAsync(async () =>
+        {
+            AppBusyService.SetBusy(ActionGetApis, true);
+
+            var response = await ApiManagementHandler.GetAllApisAsync(intent);
+
+            return response;
+
+        }, AppActionOptionPresets.Loading(ActionGetApis));
+
+        AppBusyService.SetBusy(ActionGetApis, false);
+        return DataGridResultVM<ApiDataGridVM>.New(action.Result.data ?? [], action.Result.count);
+    }
+
+    void CreateApi() => NavManager.NavigateTo("/configuration/setup/api-setup/create", true);
+    void ViewApi(ApiDataGridVM api) => NavManager.NavigateTo($"/configuration/setup/api-setup/view?ref={api.Id}", true);
+}
